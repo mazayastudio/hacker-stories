260d74b6598b7d87c4e3e6ae7d4c762d3c01b4c9
sayudha <mazaya.studio@outlook.com>
2025-02-07 17:16:10 +0700

    Add initial project setup files and dependencies.
    Include base CSS file, Vite SVG logo, and package-lock.json with project dependencies and devDependencies. This sets up the foundation for development.

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import {useEffect, useState} from "react";
+
+
+const useStorageState = (key, initialState) => {
+  const [value, SetValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem('value', value)
+  }, [value, key]);
+
+  return [value, SetValue];
+}
+
+function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
+
+  const [search, setSearch] = useStorageState('search', '')
+
+  const handleSearch = event => setSearch(event.target.value)
+  const searchStories = stories.filter(story => story.title.toLowerCase().includes(search.toLowerCase()))
+
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search
+        onSearch={handleSearch}
+        search={search}
+      />
+      <hr/>
+      <List stories={searchStories}/>
+
+    </div>
+  )
+}
+
+const Search = ({search, onSearch}) => {
+
+  return (
+    <>
+      <label htmlFor='search'>Search:</label>
+      <input
+        type='text'
+        id='search'
+        onChange={onSearch}
+        value={search}
+      />
+    </>
+  )
+}
+
+const List = ({stories}) => {
+  return (
+    <ul>
+      {stories.map(story =>
+        <Item
+          key={story.objectID}
+          story={story}
+        />
+      )}
+    </ul>
+  )
+}
+
+const Item = ({story}) => {
+  return (
+    <li>
+      <span>
+        <a href={story.url}>{story.title}</a>
+      </span>
+      <span>{story.author}</span>
+      <span>{story.num_comments}</span>
+      <span>{story.points}</span>
+    </li>
+  )
+}
+export default App
