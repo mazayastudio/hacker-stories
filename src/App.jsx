38ebe3c62fa254e55407721bbfdbd4af05a7f520
sayudha <mazaya.studio@outlook.com>
2025-02-13 09:26:03 +0700

    Refactor app to simplify state management and component hierarchy
    Removed `useStorageState` hook, replaced dynamic state management with static data for simplicity. Consolidated components and removed unnecessary logic like async data fetching, making the app more lightweight and easier to maintain.

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,62 @@
-import {useEffect, useRef, useState} from "react";
-import {useStorageState} from "./hooks/useStorageState";
-
-function App() {
-  const initialStories = [{
-    title: 'React', url: 'https://react.dev/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0
-  }, {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+const list = [
+  {
+    title       : 'React',
+    url         : 'https://react.dev/',
+    author      : 'Jordan Walke',
+    num_comments: 3,
+    points      : 4,
+    objectID    : 0,
+  },
+  {
+    title       : 'Redux',
+    url         : 'https://redux.js.org/',
+    author      : 'Jordan Walke',
     num_comments: 2,
-    points: 5,
-    objectID: 1
-  }]
-
-  const getAsyncStories = () =>
-    new Promise(resolve =>
-      setTimeout(() =>
-        resolve({data: {stories: initialStories}}), 2000))
-  const [search, setSearch] = useStorageState('search', '')
-  const [stories, setStories] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-
-  useEffect(() => {
-    setIsLoading(true)
-    getAsyncStories().then(res => {
-      setStories(res.data.stories)
-      setIsLoading(false)
-    }).catch(() => setIsError(true))
-  }, []);
-  const handleSearch = event => setSearch(event.target.value)
-  const searchStories = stories.filter(story => story.title.toLowerCase().includes(search.toLowerCase()))
-  const handleRemoveStory = item => {
-    const newStories = stories.filter(story => story.objectID !== item.objectID)
-    setStories(newStories)
+    points      : 5,
+    objectID    : 1,
   }
+]
+
+export default function App() {
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputTextWithLabel
-        id='search'
-        value={search}
-        onInputChange={handleSearch}
-      >
-        <strong>Search: </strong>
-      </InputTextWithLabel>
-      <hr/>
-      {isError && <p>Error loading stories</p>}
-      {
-        isLoading ? <p>Loading...</p> :
-          <List
-            stories={searchStories}
-            isDelete={handleRemoveStory}
-          />
-      }
+      <Search />
 
+      <hr />
+
+      <List />
     </div>
   )
 }
 
-const InputTextWithLabel = ({id, type = 'text', value, onInputChange, isFocused, children}) => {
-  const inputRef = useRef(null);
-
-  useEffect(() => {
-    (isFocused && inputRef.current) ? inputRef.current.focus() : null;
-  }, [isFocused]);
-
-  return (<>
-    <label htmlFor={id}>{children}</label>
-    <input
-      id={id}
-      type={type}
-      value={value}
-      onChange={onInputChange}
-      ref={inputRef}
-      autoFocus={isFocused}
-    />
-  </>)
+const Search = () => {
+  return (
+    <>
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" />
+    </>
+  )
 }
 
-const List = ({stories, isDelete}) => {
-  return (<ul>
-    {stories.map(story => <Item
-      key={story.objectID}
-      story={story}
-      isDelete={isDelete}
-    />)}
-  </ul>)
+const List = () => {
+  return (
+    <ul>
+      {list.map((item) => <Item key={item.objectID} item={item} />)}
+    </ul>
+  )
 }
-
-const Item = ({story, isDelete}) => {
-  return (<li>
+const Item = ({item}) => {
+  return (
+    <li>
       <span>
-        <a href={story.url}>{story.title}</a>
+        <a href={item.url}>{item.title}</a>
       </span>
-    <span>{story.author}</span>
-    <span>{story.num_comments}</span>
-    <span>{story.points}</span>
-
-    <button
-      type='button'
-      onClick={() => isDelete(story)}
-    >x
-    </button>
-  </li>)
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  )
 }
-export default App
